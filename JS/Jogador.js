6dd21b300f9f3b85b8673219b4e56e1e59596f0c
brenneralexandria <obrenneralexandria@gmail.com>
2022-11-14 20:58:50 -0300

    Funcionalidades de jogador e tabuleiro

--- JS/Jogador.js
+++ JS/Jogador.js
@@ -1,2 +1,11 @@
 // Movimentos e as marcações do tabuleiros //
 
+const PLAYER_X = "X";
+const PLAYER_O = "O";
+
+/* Para os testar os cliques dentro das celulas e validar que só será válido cliques dentro do jogo */
+document.addEventListener("click", (Event) => {
+    if(Event.target.matches(".cell")) {
+        play(Event.target.id);
+    }
+});
--- JS/Tabuleiro.js
+++ JS/Tabuleiro.js
@@ -1,2 +1,18 @@
-// As especificidades do tabuleiro e as condições de vitória //
+// As especificidades do tabuleiro e as condições de vitória.
 
+let CheckTurn = true;
+const cells = document.querySelectorAll(".cell");
+console.log(cells);
+
+// analisar de quem é a vez de jogar.
+function play(id) {
+    const cell = document.getElementById(id);
+    turn = CheckTurn ? PLAYER_X : PLAYER_O;
+    cell.textContent = turn;
+    CheckTurn = !CheckTurn;
+    CheckWin(turn);
+}
+
+// Analisar quem foi o vencedor
+function CheckWin(turn) {
+}
